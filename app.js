b32e4b261172881175fbc0d7a6527412784fa8b4
YohanpermanaRepository <yohandhikristantara99@gmail.com>
2024-06-04 20:36:19 +0700

    setup config expressjs server app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const wisatakulinerRoutes = require('./routes/routeswisatakuliner');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.use('/api', wisatakulinerRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
