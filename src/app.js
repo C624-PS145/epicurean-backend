a6fa2eaa3cdd4c0005b747485f7e4b51f223550d
YohanpermanaRepository <yohandhikristantara99@gmail.com>
2024-06-14 06:24:35 +0700

    sandbox documentation iframe

--- src/app.js
+++ src/app.js
@@ -20,20 +20,11 @@ app.use('/uploads', express.static('uploads'));
 app.use('/api', wisatakulinerRoutes);
 
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-app.get('/documentation', (req, res) => {
-  // Proxy the request to Postman
-  request('https://documenter.getpostman.com/view/34962953/2sA3XPEPE8', (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-      res.send(body);
-    } else {
-      res.status(500).send('Error fetching documentation');
-    }
-  });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+
 
 
 const PORT = process.env.PORT || 8000;
